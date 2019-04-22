97cbb5575ae7201c4dd6ae8c748a94d202362258
Hermes <miminihermes2@gmail.com>
2019-04-22 08:03:44 -0700

    first commit

---
+++ app/src/main/java/com/example/photoappv1/MyApplication.java
@@ -0,0 +1,24 @@
+package com.example.photoappv1;
+
+import android.app.Application;
+import android.net.Uri;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyApplication extends Application {
+
+    List<Uri> uriList = new ArrayList<>();
+
+    public MyApplication() {
+        this.uriList = new ArrayList<>();
+    }
+
+    public List<Uri> getUriList() {
+        return uriList;
+    }
+
+    public void setUriList(List<Uri> uriList) {
+        this.uriList = uriList;
+    }
+}
